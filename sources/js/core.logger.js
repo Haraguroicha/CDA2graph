b2b2ff1b516e8ec590c3e43f028ce682e9958ecc
腹黒い茶 <haraguroicha@hhmr.biz>
2013-02-16 14:57:18 +0800

    Make logger collapsed stacktrace

--- sources/js/core.logger.js
+++ sources/js/core.logger.js
@@ -2,15 +2,19 @@ core.logger = new function logger() {
 	this.getMsg = function getMsg(msg, printStackTrace) {
 		var date = new core.Date();
 		var st = stackTrace();
-		return sprintf("** logger[%s]:\t%s\n\t\t\t\t\t\t\t\t%s%s", date, msg, st.splice(3).join("\n\t\t\t\t\t\t\t\t"), (printStackTrace) ? "\n\t" + st.join("\n\t") : "");
+		return [sprintf("** logger[%s]:\t%s", date, msg), sprintf("\t\t\t\t\t\t\t%s%s", st.splice(3).join("\n\t\t\t\t\t\t\t\t"), (printStackTrace) ? "\n\t" + st.join("\n\t") : "")];
 	}
 	this.log = function log(msg, printStackTrace) {
-		console.log(this.getMsg(msg, printStackTrace));
+		var logMsg = this.getMsg(msg, printStackTrace);
+		logMsg[1] = logMsg[1].replace(/\n\t\t\t\t\t\t\t\t/g, "\n\t\t\t\t\t\t\t");
+		console.groupCollapsed(logMsg[0]);
+		console.log(logMsg[1]);
+		console.groupEnd();
 	}
 	this.warn = function warn(msg, printStackTrace) {
-		console.warn(this.getMsg(msg, printStackTrace));
+		console.warn(this.getMsg(msg, printStackTrace).join("\n\t\t\t\t\t\t\t\t"));
 	}
 	this.error = function error(msg, printStackTrace) {
-		console.error(this.getMsg(msg, printStackTrace));
+		console.error(this.getMsg(msg, printStackTrace).join("\n\t\t\t\t\t\t\t\t"));
 	}
 };
